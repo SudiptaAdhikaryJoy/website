d1bb3901d6d2cb780f595f3a2da34bc700a61288
tomadeytania <tomadeytania10@gmail.com>
2021-07-19 23:39:15 +0600

    7th

---
+++ script.js
@@ -0,0 +1,10 @@
+<a button onclick="document.location='default.asp'">District Details</button>
+    <a button onclick="document.location='default.asp'">Govt.Offices</button>
+    <a button onclick="document.location='default.asp'">Govt.Education Institue</button>
+    <a button onclick="document.location='default.asp'">Photo Grallary</button>
+    <a button onclick="document.location='default.asp'">Deputy Commission</button>
+    <a button onclick="document.location='default.asp'">Local Govt.</button>
+    <a button onclick="document.location='default.asp'">E-Service</button>
+    <a button onclick="document.location='default.asp'">Tour Guide</button>
+    <a button onclick="document.location='default.asp'">Temple</button>
+    <a button onclick="document.location='default.asp'">MOSQUE</a button>
